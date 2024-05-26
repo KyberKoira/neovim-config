f2db2b71631e640139305efda4c5ad1c70478451
Joel <you@example.com>
2024-05-26 12:46:37 +0300

    Added stuff

--- init.lua
+++ init.lua
@@ -531,6 +531,36 @@ mason_lspconfig.setup_handlers {
   end,
 }
 
+require("toggleterm").setup{
+  -- size can be a number or function which is passed the current terminal
+  size = 20,
+  open_mapping = [[<c-\>]],
+  hide_numbers = true, -- hide the number column in toggleterm buffers
+  autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+  shade_terminals = false, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+  close_on_exit = true, -- close the terminal window when the process exits
+   -- Change the default shell. Can be a string or a function returning a string
+  shell = vim.o.shell,
+  insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+  auto_scroll = true, -- automatically scroll to the bottom on terminal output
+  -- This field is only relevant if direction is set to 'float'
+  float_opts = {
+    -- The border key is *almost* the same as 'nvim_open_win'
+    -- see :h nvim_open_win for details on borders however
+    -- the 'curved' border is a custom border type
+    -- not natively supported but implemented in this plugin.
+    border = 'single',
+    -- like `size`, width, height, row, and col can be a number or function which is passed the current terminal
+  },
+  winbar = {
+    enabled = false,
+    name_formatter = function(term) --  term: Terminal
+      return term.name
+    end
+  },
+}
+
 vim.keymap.set('n', '<CR>', 'm`o<Esc>``')
 vim.keymap.set('n', '<S-CR>', 'm`O<Esc>``')
 
@@ -546,6 +576,8 @@ vim.keymap.set('n','<leader><Left>', ':-tabnext<Enter>')
 vim.keymap.set('n','<leader><Right>', ':+tabnext<Enter>')
 vim.keymap.set('n','<leader>t', ':tabnew<Enter>')
 vim.keymap.set('n','<leader>tc', ':tabclose<Enter>')
+
+-- Terminal Interaktiot
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- For local replace
